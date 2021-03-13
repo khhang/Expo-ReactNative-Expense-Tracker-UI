ee66c76f45dc4b89e404a05d1eba1a1dda1a9b51
Brian <13318505+khhang@users.noreply.github.com>
2021-03-12 17:54:45 -0800

    removed dead code

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer'; 
 import React, { useEffect, useState } from 'react';
 import { StatusBar, View } from 'react-native';
-import {dropExpenseTables, initializeTables} from './services/sqlite-service';
+import { initializeTables} from './services/sqlite-service';
 
 import HomeStackNavigator from './navigation/HomeStackNavigator';
 import AccountsStackNavigator from './navigation/AccountsStackNavigator';
@@ -11,53 +11,17 @@ import CategoriesStackNavigator from './navigation/CategoriesStackNavigator';
 import ExpensesStackNavigator from './navigation/ExpensesStackNavigator';
 import ExpenseAnalysisStackNavigator from './navigation/ExpenseAnalysisStackNavigator';
 
-import {categoriesService} from './services/categories-service';
-import {accountsService} from './services/accounts-service';
-import {expensesService} from './services/expenses-service';
-
 const Drawer = createDrawerNavigator();
 
 const App = () => {
 
   useEffect(() => {
-    const seedData = async() => {
-      // Categories
-      await categoriesService.addCategory('Transportation');
-      await categoriesService.addSubcategory(1, 'Car');
-      await categoriesService.addSubcategory(1, 'Uber');
-      await categoriesService.addSubcategory(1, 'Gas');
-      await categoriesService.addCategory('Groceries');
-      await categoriesService.addSubcategory(2, 'Vons');
-      await categoriesService.addSubcategory(2, 'Ralphs');
-      await categoriesService.addCategory('Food');
-      await categoriesService.addSubcategory(3, 'Restaraunsolts');
-      await categoriesService.addSubcategory(3, 'Uber Eats');
-      await categoriesService.addCategory('Entertainment');
-      await categoriesService.addSubcategory(4, 'Movies');
-      await categoriesService.addSubcategory(4, 'Computer');
-      await categoriesService.addSubcategory(4, 'Games');
-      await categoriesService.addSubcategory(4, 'AirBnB');
-
-      // Accounts
-      await accountsService.addAccount('Chase Credit', '100.50');
-      await accountsService.addAccount('HSBC Debit', '69.69');
-
-      // Expenses
-      await expensesService.addExpense(1, 1, 'Car Payment', -25.60, 1, new Date().toISOString());
-      await expensesService.addExpense(2, 4, 'Vons groceries', -62.10, 1, new Date().toISOString());
-      await expensesService.addExpense(1, 1, 'Servicing', 100.81, 1, new Date().toISOString());
-      await expensesService.addExpense(1, 2, 'Uber', -55.29, 2, new Date().toISOString());
-      await expensesService.addExpense(2, null, 'Sheba', -62.10, 1, new Date().toISOString());
-      await expensesService.addExpense(4, 10, null, 62.10, 1, new Date().toISOString());
-    }
 
     const init = async() => {
-      // await dropExpenseTables();
       await initializeTables();
     }
 
     init();
-    // seedData();
   }, []);
   
   const [currRoute, setCurrRoute] = useState('Home');
